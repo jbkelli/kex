fecde2f60506170bafe3edba82eefb0af6ecb340
jbkelli <444mwangialvinm@gmail.com>
2024-10-01 01:23:29 +0300

    statment.c

---
+++ statement.c
@@ -0,0 +1,37 @@
+//Program to evaluate loan eligibility from various users
+/*Program to evaluate loan eligibility from various users
+Author: Alvin Maina Mwangi
+Date: 29th September 2024
+*/
+
+//Introduce the basic C outline
+#include<stdio.h>
+int main () {
+	
+	//Assign the various data types
+	int age;
+	double income;
+	
+	//Print statement to enquire age details from the user
+	printf("Enter your age: ");
+	scanf("%d", &age);
+	
+	//Print a statement to enquire annual income details from the user
+	printf("Enter your income(annual): ");
+	scanf("%lf", &income);
+	
+	//Introduce if statement to group data according to required specifications
+	if(age >= 21 | income >= 21000) {
+		
+		//Print the result if the required specifications are met
+		printf("Congratulations, you qualify for a loan.");
+	}
+	
+	if(age < 21 | income < 21000) {
+		
+		//Print the result if the required specifications are not met
+		printf("Unfortunately, we are unable to offer you a loan at this time.");
+	}
+	
+	return 0;
+}
