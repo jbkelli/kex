49595ff2a1b669d6169837f65ac848d3a1947b00
jbkelli <444mwangialvinm@gmail.com>
2024-10-02 15:22:00 +0300

    statement.c complete project

--- statement.c
+++ statement.c
@@ -21,16 +21,16 @@ int main () {
 	scanf("%lf", &income);
 	
 	//Introduce if statement to group data according to required specifications
-	if(age >= 21 | income >= 21000) {
+	if(age >= 21 && income >= 21000) {
 		
 		//Print the result if the required specifications are met
-		printf("Congratulations, you qualify for a loan.");
+		printf("Congratulations, you qualify for a loan.\n");
 	}
 	
-	if(age < 21 | income < 21000) {
+	else{
 		
 		//Print the result if the required specifications are not met
-		printf("Unfortunately, we are unable to offer you a loan at this time.");
+		printf("Unfortunately, we are unable to offer you a loan at this time.\n");
 	}
 	
 	return 0;
