0de495317925f72ced09659288adead89c7f9b34
jbkelli <444mwangialvinm@gmail.com>
2024-10-30 13:50:44 +0300

    create file

--- Grinderint.c
+++ Grinderint.c
@@ -1,33 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	char name[30];
-	float marks;
-	FILE *fptr;
-	
-	fptr = fopen("C:\\massvc\\grinder.txt","w");
-	
-	if(fptr==NULL){
-		printf("Cannot access file");
-		
-		exit(1);
-	}
-	else
-	{
-		printf("File access successful \n");
-	}
-	
-	printf("Enter students name: ");
-	scanf(" %[^\n]", &name);
-	
-	printf("Enter student marks: ");
-	scanf("%f", &marks);
-	
-	fprintf(fptr,"%s, \n%f", name, marks);
-	
-	
-	fclose(fptr);
-	
-	return 0;
+    char name[30];
+    float marks;
+    FILE *fptr;
+    int i;
+
+    // Open the file in write mode
+    fptr = fopen("students.txt", "w");
+    if (fptr == NULL) {
+        printf("Error opening file!");
+        exit(1);
+    }
+
+    // Collect and write data for 5 students
+    for(i = 0;i < 5;i++) {
+        printf("Enter name of student %d: ", i + 1);
+        scanf(" %[^\n]", name);  // Read full name including spaces
+
+        printf("Enter marks of student %d: ", i + 1);
+        scanf("%f", &marks);
+
+        // Write to file in the format: Name Marks
+        fprintf(fptr, "%s %.2f\n", name, marks);
+    }
+
+    printf("Data successfully written to file.\n");
+
+    // Close the file
+    fclose(fptr);
+
+    return 0;
 }
